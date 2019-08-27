ed45771613636c96ff1349cd99bd2c565586af68
Angkarn Janjuang <angkarn9@gmail.com>
2019-08-27 22:06:38 +0700

    add feature update

--- src/app/todo-manage/todo-manage.component.ts
+++ src/app/todo-manage/todo-manage.component.ts
@@ -12,6 +12,8 @@ export class TodoManageComponent implements OnInit {
   todoList: Todo[];
   todoForm: FormGroup;
   errorMessage = '';
+  id: number;
+
   constructor(
     private formBuilder: FormBuilder
   ) { }
@@ -19,9 +21,13 @@ export class TodoManageComponent implements OnInit {
   ngOnInit() {
     this.todoList = [];
     this.todoForm = this.formBuilder.group({
-      topic: [''],
-      description: ['']
+      topic: ['', Validators.required],
+      description: ['', Validators.required]
     });
+  }
+
+  private genId(): number {
+    return this.todoList.length + 1;
   }
 
   add() {
@@ -32,10 +38,33 @@ export class TodoManageComponent implements OnInit {
       this.errorMessage = 'topic is require field';
     }
     const todo = new Todo({
-      id: 1,
+      id: this.genId(),
       topic,
       description
     });
     this.todoList.push(todo);
+    this.todoForm.reset();
+  }
+
+  edit(item: Todo) {
+    this.todoForm.get('topic').patchValue(item.topic);
+    this.todoForm.get('description').patchValue(item.description);
+    this.id = item.id;
+  }
+
+  update() {
+    const todo = this.todoList.find(item => item.id === this.id);
+    todo.topic = this.todoForm.get('topic').value;
+    todo.description = this.todoForm.get('description').value;
+    this.id = undefined;
+    this.todoForm.reset();
+  }
+
+  remove(id: number) {
+    this.todoList.forEach((item: Todo, index: number) => {
+      if (id === item.id) {
+        this.todoList.splice(index, 1);
+      }
+    });
   }
 }
